0e6a10a1727156a0b273eca560e68e4002672973
Darren Ng <darren.ng@ucdconnect.ie>
2015-11-27 18:54:46 +0000

    Jelly ver 2.2 Changes log:
    ~This is almost finished.

--- app/src/main/java/com/example/darre_000/scoutr/SettingsActivity.java
+++ app/src/main/java/com/example/darre_000/scoutr/SettingsActivity.java
@@ -1,9 +1,13 @@
 package com.example.darre_000.scoutr;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -12,19 +16,71 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        Button favourites = (Button) findViewById(R.id.menuFavourites);
+        favourites.setOnClickListener(favouritesListener);
+
+        Button radius = (Button) findViewById(R.id.menuLoadRadius);
+        radius.setOnClickListener(radiusListener);
+
+        Button accout = (Button) findViewById(R.id.menuAccount);
+        accout.setOnClickListener(accountListener);
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.menuCheckBox);
+        checkBox.setOnClickListener(boxListener);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // TODO Auto-generated method stub
+    private View.OnClickListener favouritesListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+            builder.setMessage("If favourites was working on the local database this would clear the list.")
+                    .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
+        }
+    };
 
-        menu.add("menu1");
-        menu.add("menu2");
-        menu.add("menu3");
-        menu.add("menu4");
+    private View.OnClickListener radiusListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+            builder.setMessage("This would allow the user to set the radius at which nearby pin will be loaded.")
+                    .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
+        }
+    };
 
-        return super.onCreateOptionsMenu(menu);
-    }
+    private View.OnClickListener accountListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+            builder.setMessage("Would show user's account details.")
+                    .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
+        }
+    };
+
+    private View.OnClickListener boxListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+            builder.setMessage("As the database is local photos must be save to the phone so this checkBox is purly astectic.")
+                    .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).show();
+        }
+    };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
